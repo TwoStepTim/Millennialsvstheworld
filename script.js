370c965ecb15edfe3911e42b21c9f6f0cd52310f
TwoStepTim <tjmims33@gmail.com>
2023-09-23 15:34:01 -0400

    Init  Code

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Toggle the sidebar when the hamburger is clicked
+    $(".hamburger").click(function() {
+      $(".sidebar").toggleClass("open");
+    });
+  });
